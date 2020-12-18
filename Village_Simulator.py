61fcae524b85037dd9d514d7d57b5501c410e72b
LouisLu705 <54871600+LouisLu705@users.noreply.github.com>
2020-12-18 16:04:16 -0500

    Update Village_Simulator.py
    Added player class
    Added main function to be used as driver code for the rest of the game

--- Village_Simulator.py
+++ Village_Simulator.py
@@ -6,32 +6,40 @@ A text based resource management game. There is no pressure to min-max this is m
 Author: Louis Lu || Date: 12/11/2020
 """
 
-flag = 0
-scene = 0
+import random
 
-def move():
-    if (flag==0):
-        opening_text = "\nAdventurer, your decision in movement is of the utmost importance!\n"
-        opening_text+= "Please let me know when you have decided so that I may go back to my family..."
-        opening_text+= "it has been several years since the last adventurer made a move, and I am weary."
-        opening_text+= " My bones have long grown tired and my soul desperately wishes to sleep..."
-        opening_text+= "\n\nGrant an old soul their last wish? (Enter North, East, South, West)"
-    print(opening_text)
-    direction = input()
-    direction=direction.lower()
-    if (direction == "north"):
-        print("joy")
-    elif (direction == "east"):
-        print("joy")
-    elif (direction == "south"):
-        print("joy")
-    elif (direction == "west"):
-        print("joy")
-    elif (direction == "no"):
-        if (scene==0):
-            scene_text = "The last adventurer said the same thing but I've learned since then..."
-            scene_text+= "\nNone of you can be trusted so I've come up with a few tricks just in case."
-            scene_text+= "\n\n You Died"
+class Player:
+    def __init__(self):
+    # Create a new character    
+        self.health = (random.randint(0,20))
+        self.attack = (random.randint(0,20))
+        self.luck = (random.randint(0,20))
+        self.defense = (random.randint(0,20))
+        self.speed = (random.randint(0,20))
+        
+        
+        
+    def move(self):
+        while (True):
+            exit = False
+            print("Please enter a direction: (North, East, South, West)")
+            direction = input()
+            lowercase_dir = direction.lower()
+            if (direction == "north"):
+                self.move = lowercase_dir
+                break
+            elif (direction == "east"):
+                self.move = lowercase_dir
+                break
+            elif (direction == "south"):
+                self.move = lowercase_dir
+                break
+            elif (direction == "west"):
+                self.move = lowercase_dir
+                break
+            else:
+                print("unknown direction {} entered".format(direction))
+                            
 
 def print_current_state(current_state):
     print('-'*22)
@@ -47,17 +55,56 @@ def print_current_state(current_state):
         if (rows==2 or rows==5):
             print('-'*22)
     print('-'*22)
-    
-#Print out the start state of the map
-current_state = [ [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-                  [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-                  [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-                  [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-                  [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-                  [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-                  [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-                  [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-                  [ '.', '.', '.', '.', '.', '.', '.', '.', '.'] ]
 
-print_current_state(current_state)
-move()
+def start():
+    """
+    Start the introduction text to introduce the basics of the game to the player
+    """
+    name = '\x1b[36m' + "Village Simulator" + '\x1b[0m'
+    print("Welcome to {}!".format(name),end = "\n\n")
+    
+    print("The smog clouds slowly drift up into the air as you stare aimlessly at the night sky. "  
+          "Like clockwork the factory doesn't stop. Loud sounds continue to ring out "
+          "from within the compound as you reminisce about where you spent your youth."
+          "Time doesn't stop for anyone and slowly all the people you once knew left. Finally your time has come. "
+          "With that you step away from the doors and leave without looking back. ",end="\n\n")
+    
+    print("A few months pass and you find yourself in the middle of an Australian forest. "
+          "The air is clean and outside the sun is shining. The sun illuminates your slightly "
+          "disheveled face but plastered on is an undeniable smile. You've never felt more relaxed "
+          "and now you have finally sealed the deal. This land is yours and you are free to build it up "
+          "into what you've always dreamed of.",end="\n\n")
+    
+    print("As you begin thinking about the future a bright light suddenly appears in front of you. "
+          "In a flash you are consumed by the light and mere moments later find yourself sitting on the "
+          "ground of an unfamiliar land. Looking up into the sky you see 3 suns and a roar sounds out as "
+          "a massive dragon seems to have just flown by overhead. Confused and lost you remember the last words " 
+          "the person who sold this land to you said. \"Good luck\" You stand up and ponder briefly about whether "
+          "he knew all along. Seeing as you may never know the truth a level of grim determination you didn't know you were capable of "
+          "fills you as you take your first steps towards a firmer future. Although you didn't know it this is the beginning "
+          "of how this world would change forever...")
+    
+    
+    
+def main():
+    start()
+    
+    current_state = [ [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                      [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                      [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                      [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                      [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                      [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                      [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                      [ '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                      [ '.', '.', '.', '.', '.', '.', '.', '.', '.'] ]    
+    
+    Character = Player()
+    
+
+"""
+Beginning driver code
+"""
+flag = 0
+scene = 0
+main()
