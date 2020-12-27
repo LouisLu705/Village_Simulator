d1f1fa787d2bbce48b10aa8d442442f27ab01962
LouisLu705 <54871600+LouisLu705@users.noreply.github.com>
2020-12-27 10:55:22 -0500

    Update Village_Simulator.py
    Added infinite boards.
    - A positive y map_value represents crossing the border by going North
    - A negative y map_value represents
    crossing the border by going South
    - A positive x map_value represents crossing the border by going West
    - A negative x map_value represents
    crossing the border by going East
    
    Added additional ability to move:
    - 'n' = "North"
    - 's' = "South"
    - 'e' = "East"
    - 'w' = "West
    Note also made sure by converting all input to lower nothing is case sensitive

--- Village_Simulator.py
+++ Village_Simulator.py
@@ -7,6 +7,7 @@ Author: Louis Lu || Date: 12/11/2020
 """
 
 import random
+import sys
 
 class Player:
     def __init__(self):
@@ -18,7 +19,7 @@ class Player:
         self.speed = (random.randint(0,20))
             
         
-    def move(self, board):
+    def move(self, board, all_boards):
         print_bd = True
         while (True):
             if (print_bd):
@@ -27,24 +28,116 @@ class Player:
             print("\nPlease enter a direction: (North, East, South, West)")
             direction = input()
             lowercase_dir = direction.lower()
-            if (lowercase_dir == "north"):
+            if (lowercase_dir == "north" or lowercase_dir == 'n'):
                 board.current_state[board.y][board.x]='.'
                 board.y=board.y-1
-            elif (lowercase_dir == "east"):
+            elif (lowercase_dir == "east" or lowercase_dir == 'e'):
                 board.current_state[board.y][board.x]='.'
                 board.x=board.x-1
-            elif (lowercase_dir == "south"):
+            elif (lowercase_dir == "south" or lowercase_dir == 's'):
                 board.current_state[board.y][board.x]='.'
                 board.y=board.y+1
-            elif (lowercase_dir == "west"):
+            elif (lowercase_dir == "west" or lowercase_dir == 'w'):
                 board.current_state[board.y][board.x]='.'
                 board.x=board.x+1
             elif (lowercase_dir == "exit"):
-                break
+                sys.exit("Thank you for playing {}.".format(name))
             else:
                 print("unknown direction \'{}\' entered".format(direction))
                 print_bd = False
+                
+            global map_value, last_map_value
+            old_x = board.x
+            old_y = board.y
+            #Let the even map_values hash to the bd_states which cross the x bounds
+            #Let the odd map_values hash to the bd_states which cross the y bounds
+            if (board.y < 0):
+                if (map_value <= 1):
+                    last_map_value = map_value
+                    if (map_value < 0):
+                        map_value=2*map_value-1
+                    else:
+                        map_value = -2*map_value-1
+                    if (map_value in all_boards):
+                        board = all_boards[map_value]
+                        print("y1 is less than 0 with map_val: {}".format(map_value))
+                    else:
+                        board = Board()
+                        board.current_state[board.y][board.x]='.'
+                        all_boards[map_value]=board
+                elif (map_value > last_map_value):
+                    map_value = last_map_value
+                    last_map_value = (map_value-1)/2
+                    board = all_boards[map_value]
+                    print("y2 is less than 0 with map_val: {}".format(map_value))
+                board.y = 8
+                board.x = old_x
+            elif (board.y > 8):
+                if (map_value >=1):
+                    last_map_value = map_value
+                    map_value=2*map_value+1
+                    if (map_value in all_boards):
+                        board = all_boards[map_value]
+                        print("y3 is greater than 8 with map_val: {}".format(map_value))
+                    else:
+                        board = Board()
+                        board.current_state[board.y][board.x]='.'
+                        all_boards[map_value]=board
+                elif (map_value < last_map_value):
+                    map_value = last_map_value
+                    if ((map_value+1)/2 == -1):
+                        last_map_value = 1
+                    else:
+                        last_map_value = (last_map_value+1)/2
+                    board = all_boards[map_value]
+                    print("y4 is greater than 8 with map_val: {}".format(map_value))
+                board.y = 0
+                board.x = old_x
+            elif (board.x < 0):
+                if (map_value <=1):
+                    last_map_value = map_value
+                    if (map_value < 0):
+                        map_value=2*map_value
+                    else:
+                        map_value=-2*map_value
+                    if (map_value in all_boards):
+                        board = all_boards[map_value]
+                    else:
+                        board = Board()
+                        board.current_state[board.y][board.x]='.'
+                        all_boards[map_value]=board
+                elif (map_value > last_map_value):
+                    map_value = last_map_value
+                    last_map_value = map_value/2
+                    board = all_boards[map_value]
+                    #debug print statement
+                    #print("x1 is less than 0 with map_val: {}".format(map_value))
+                board.x = 8
+                board.y = old_y
+            elif (board.x > 8):
+                if (map_value >= 1):
+                    last_map_value = map_value
+                    map_value=2*map_value
+                    if (map_value in all_boards):
+                        board = all_boards[map_value]
+                    else:
+                        board = Board()
+                        board.current_state[board.y][board.x]='.'
+                        all_boards[map_value]=board
+                elif (map_value < last_map_value):
+                    map_value = last_map_value
+                    if (map_value/2 == -1):
+                        last_map_value = 1
+                    else:
+                        last_map_value = last_map_value/2
+                    board = all_boards[map_value]
+                    #debug print statement
+                    #print("x3 is greater than 8 with map_val: {}".format(map_value))
+                    
+                board.x = 0
+                board.y = old_y
             board.current_state[board.y][board.x]='P'
+            print(all_boards)
                             
 class Board:
     def __init__(self):
@@ -63,6 +156,15 @@ class Board:
         #Note the first element in a 2d list represents the "row" so the first "[]" 
         #is the y position in the board state.
         self.current_state[self.y][self.x]='P'
+        
+        rng_range = random.randint(10,25)
+        for rng in range(rng_range):
+            rng_x = random.randint(0,8)
+            rng_y = random.randint(0,8)
+            if (rng_x == self.x and rng_y == self.y):
+                continue
+            else:
+                self.current_state[rng_y][rng_x] = '^'
         
         
     def print_current_state(self):
@@ -84,7 +186,6 @@ def start():
     """
     Start the introduction text to introduce the basics of the game to the player
     """
-    name = '\x1b[36m' + "Village Simulator" + '\x1b[0m'
     print("Welcome to {}!".format(name),end = "\n\n")
     
     print("The smog clouds slowly drift up into the air as you stare aimlessly at the night sky. "  
@@ -113,24 +214,31 @@ def start():
 def main():
     # print out start() text to introduce the player to the setting
     start()  
-    
-    # board and player object for the rest of the instance
-    bd_state = Board()
-    Character = Player()
         
+    all_boards = {}
+    all_boards[map_value] = board
+
     
     while (True):
         cmd=input()
         
         if (cmd == "exit"):
-            break
+            sys.exit("Thank you for playing {}.".format(name))
         elif (cmd == "move"):
-            Character.move(bd_state)
-        
-        
+            Character.move(board, all_boards)
+   
+
 """
-Beginning driver code
+Global Variables
 """
 flag = 0
 scene = 0
+map_value = 1  
+name = '\x1b[36m' + "Village Simulator" + '\x1b[0m'
+last_map_value = 1  
+board = Board()
+Character = Player() 
+"""
+Beginning driver code
+"""
 main()
